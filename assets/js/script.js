73f222e48eee5bf95705488e0431422000d4ea59
biasp <94643456+Biaasp@users.noreply.github.com>
2023-10-01 09:53:40 +0200

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+const hamburgerIcon = document.querySelector('.nav__hamburger__icon');
+const hamburgerMenu = document.querySelector('.nav__hamburger__menu');
+const header = document.querySelector('header')
+hamburgerIcon.addEventListener("click", function(){ 
+    hamburgerMenu.classList.toggle("active")
+    header.classList.toggle("active")
+});
+
+document.querySelectorAll(".nav__link").forEach(n => n.addEventListener("click", () => {
+    hamburgerMenu.classList.remove("active")
+    header.classList.remove("active")
+}))
